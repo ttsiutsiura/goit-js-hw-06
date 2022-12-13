79575e0fc539149562e6f01d3fd831502373ad74
Kirill Tsiutsiura <kirill.tsiutsiura7@gmail.com>
2022-12-13 13:17:52 +0200

    Последнее задание

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,41 @@
+const inputEl = document.querySelector("#controls").firstElementChild;
+const createBtnEl = document.querySelector("button[data-create]");
+const destroyBtnEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+let collection = [];
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  for (let i = 0; i < inputEl.value; i++) {
+    const boxEl = document.createElement("div");
+
+    collection.push(boxEl);
+  }
+
+  collection[0].style.backgroundColor = getRandomHexColor();
+  collection[0].style.width = "30px";
+  collection[0].style.height = "30px";
+
+  for (let i = 1; i < collection.length; i++) {
+    collection[i].style.width =
+      Number.parseInt(collection[i - 1].style.width) + 10 + "px";
+    collection[i].style.height =
+      Number.parseInt(collection[i - 1].style.height) + 10 + "px";
+
+    collection[i].style.backgroundColor = getRandomHexColor();
+  }
+
+  boxesEl.append(...collection);
+}
+
+function destroyBoxes() {
+  document.querySelector("#boxes").innerHTML = "";
+  collection = [];
+}
+
+createBtnEl.addEventListener("click", createBoxes);
+destroyBtnEl.addEventListener("click", destroyBoxes);
